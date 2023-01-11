c220b75cbc843fa6d81819fcefaa48175db3d930
nuelxelence <nuelxelence@gmail.com>
2023-01-11 18:38:03 +0100

    file deletion

--- programm/views.py
+++ programm/views.py
@@ -5,5 +5,5 @@ from .models import *
 def index(request):
     obj = Motivation.objects.all()
     context={'obj':obj}
-    return render(request, 'hme.html',context)
-    # return render(request, 'general/index.html',context)
+    return render(request, 'general/home.html',context)
+   
