2ecf12ebf8d7aa0ec97e557854a180e91e3c264a
nuelxelence <nuelxelence@gmail.com>
2023-01-16 14:17:13 +0100

    static files

--- churchProject/settings.py
+++ churchProject/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #'whitenoise.runserver_nostatic',
+
       'programm',
    
 
@@ -61,12 +63,17 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    #"whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+  
+
 ]
 
 ROOT_URLCONF = 'churchProject.urls'
@@ -141,6 +148,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR ,'static') ]
 STATIC_ROOT =os.path.join(BASE_DIR ,'staticfiles')  
+
+#STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR ,'media/')
