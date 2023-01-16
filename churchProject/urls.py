a88ca0b49a19adc2e496241259a2156b4f653e8d
nuelxelence <nuelxelence@gmail.com>
2023-01-16 14:47:26 +0100

    static files

--- churchProject/urls.py
+++ churchProject/urls.py
@@ -23,9 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    #path('account/',include('account.urls')),
+   
     path('',include('programm.urls')),
-    #path('partners/',include('partners.urls')),
+ 
   
    
 ]
