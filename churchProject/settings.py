42a1e10c97d4ee82bb280597b47ccec8e1fe10d9
nuelxlencedjango <nuelxlence@gmail.com>
2023-09-23 08:07:22 +0100

    gallery

--- churchProject/settings.py
+++ churchProject/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
 
       'programm',
       'accounts',
